760919bf5732f2725f216821ffc6cfb1cd1fc3ea
Douglas Antonio <douglas.tony1235@gmail.com>
2025-08-23 15:58:35 -0300

    mudança no git ignore

--- cypress/e2e/3-loja-ebac-testes/produtos.cy.js
+++ cypress/e2e/3-loja-ebac-testes/produtos.cy.js
@@ -1,19 +1,41 @@
 /// <reference types="cypress"/>
-
+import produtosPage from "../../support/page-objects/produtos.page";
 describe('Funcionalidade: Produtos ', () => {
 
-beforeEach(() => {
-    cy.visit('produtos')
-    
-});
+    beforeEach(() => {
+        produtosPage.visitarUrl()
+
+    });
 
     it('deve selecionar um produto da lista', () => {
-        cy.get(' .product-block ')
-        //.first()
-        //.last()
-        .contains('Apollo Running Short')
-        .click()
-        cy.get('#tab-title-description > a').should('contain' , "Descrição")
-        
+        produtosPage.buscarProdutoLista('Augusta Pullover Jacket')
+        cy.get('#tab-title-description > a').should('contain', "Descrição")
+
+    });
+
+    it('Deve buscar um produto com sucesso', () => {
+        let produto = 'Zeppelin Yoga Pant'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
+
+    });
+
+    it('Deve visitar a pagina do produto', () => {
+        produtosPage.visitarProduto('Zeppelin-Yoga-Pant')
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+        let qtd = 7
+        produtosPage.visitarProduto('Arcadio Gym Short')
+        produtosPage.addProdutoCarrinho('34', 'Blue', qtd)
+        cy.get('.woocommerce-message').should('contain', `${qtd} × "Arcadio Gym Short" foram adicionados no seu carrinho.`)
+    });
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+            produtosPage.visitarProduto(dados[0].nomeProduto)
+            produtosPage.addProdutoCarrinho(dados[0].tamanho, dados[0].cor, dados[0].quantidade)
+            cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+        })
     });
 });
